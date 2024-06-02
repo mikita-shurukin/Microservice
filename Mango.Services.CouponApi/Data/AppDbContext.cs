e150aea7ef3144ce7bf54e701d4e77853f7d34e2
mikita-shurukin <116252264+mikita-shurukin@users.noreply.github.com>
2024-06-02 20:55:04 +0200

    Add project files.

---
+++ Mango.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "O19FC",
+                DiscountAmount = 10,
+                MinAmount = 20,
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "O145C",
+                DiscountAmount = 0,
+                MinAmount = 0,
+            });
+        }
+    }
+}
---
+++ Mango.Services.CouponApi/Migrations/20240530130827_SeedCouponTables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.CouponApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCouponTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "LastUpdated", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "O19FC", 10.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 20 },
+                    { 2, "O145C", 0.0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+        }
+    }
+}
